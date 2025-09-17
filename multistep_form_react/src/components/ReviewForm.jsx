8a7cafcbf3af0ba07dc48ae1b382d28a0a436584
Jorge <jorgeappontes13@gmail.com>
2025-09-17 10:26:32 -0300

    estilos

--- multistep_form_react/src/components/ReviewForm.jsx
+++ multistep_form_react/src/components/ReviewForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './reviewform.css'
 
 const ReviewForm = () => {
   return (
--- multistep_form_react/src/components/thanks.jsx
+++ multistep_form_react/src/components/thanks.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './thanks.css'
 
 const thanks = () => {
   return (

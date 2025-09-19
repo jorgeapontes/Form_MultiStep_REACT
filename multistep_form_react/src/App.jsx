88a1834915e64f278cf740c69d01039f5831e926
Jorge <jorgeappontes13@gmail.com>
2025-09-18 21:37:18 -0300

    finalizando

--- multistep_form_react/src/App.jsx
+++ multistep_form_react/src/App.jsx
@@ -1,63 +1,79 @@
-import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
-import { FiSend } from 'react-icons/fi'
-import UserForm from './components/UserForm'
-import ReviewForm from './components/ReviewForm'
-import Thanks from './components/Thanks'
-import React, { useState } from 'react'
-import './App.css'
+// Components
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
-// hooks
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
 
-import { useForms } from './hooks/useForms'
+// Hooks
+import { useState } from "react";
+import { useForms } from "./hooks/useForms";
+
+// CSS
+import "./App.css";
+
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(formTemplate);
 
-
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return { ...prev, [key]: value };
+    });
+  };
 
   const formComponents = [
-    <UserForm />,
-    <ReviewForm />,
-    <Thanks />
-  ]
+    <UserForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <Thanks data={data} />,
+  ];
 
-  const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = useForms(formComponents)
+  const { currentStep, currentComponent, changeStep, isLastStep } =
+    useForms(formComponents);
 
   return (
-    <>
-      <div className="app">
-        <div className="header">
-          <h2>Deixe sua avaliação!</h2>
-          <p>ficamos felizes com a sua compra, use o formulário para avaliar sua compra!</p>
-        </div>
-        <div className="form-container">
-          <p>etapas</p>
-          <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
-            <div className="inputs-container">
-              {currentComponent}
-            </div>
-            <div className="actions">
-              {!isFirstStep && (
-                <button type="button" onClick={(e) => changeStep(currentStep - 1, e)}>
-                  <GrFormPrevious />
-                  <span>Voltar</span>
-                </button>
-              )}
-              {!isLastStep ? (
-                <button type="submit">
-                  <span>Avançar</span>
-                  <GrFormNext />
-                </button>
-              ): (<button type="button">
-                  <span>Enviar</span>
-                  <FiSend />
-                </button>)}
-            </div>
-          </form>
-        </div>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
       </div>
-    </>
-  )
+      <div className="form-container">
+        <Steps currentStep={currentStep} />
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          <div className="inputs-container">{currentComponent}</div>
+          <div className="actions">
+            <button type="button" onClick={() => changeStep(currentStep - 1)}>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- multistep_form_react/src/components/UserForm.jsx
+++ multistep_form_react/src/components/UserForm.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
-const UserForm = () => {
+const UserForm = ({ data, updateFieldHandler }) => {
   return (
-    <div><h2>UserForm</h2></div>
-  )
-}
+    <div>
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite o seu nome"
+          required
+          value={data.name || ""}
+          onChange={(e) => updateFieldHandler("name", e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="name">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o seu e-mail"
+          required
+          value={data.email || ""}
+          onChange={(e) => updateFieldHandler("email", e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default UserForm
+export default UserForm;
